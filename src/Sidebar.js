f684d4e8a7567468428b695f9d6251bcd4bea1df
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-05 23:12:56 +0300

    Add header to the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Sidebar.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { IconButton, Avatar } from '@material-ui/core';
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h2>This is Sidebar</h2>
+            <div className="sidebar__header">
+                <Avatar src="https://cdn.pixabay.com/photo/2017/04/30/11/42/hijab-2272708_960_720.png"/>
+                <div className="sidebar_headerRight">
+                   <IconButton>
+                     <DonutLargeIcon/>
+                   </IconButton>
+                   <IconButton>
+                     <ChatIcon  />
+                   </IconButton>
+                   <IconButton>
+                     <MoreVertIcon/>
+                   </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
