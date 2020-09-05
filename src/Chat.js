a898e19c28354b6cbcbdc2e9277a60b76044c8a3
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-05 22:28:25 +0300

    Initial Project setup

---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css';
+function Chat() {
+    return (
+        <div className="chat">
+            <h2>This is Chat</h2>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Sidebar.css';
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h2>This is Sidebar</h2>
+        </div>
+    )
+}
+
+export default Sidebar
